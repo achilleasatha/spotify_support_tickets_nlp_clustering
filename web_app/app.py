9a97d2960937d0aa393ee67367a4b6d58d177703
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2020-03-13 01:57:08 +0000

    data wrangling for prediction time and dict for nice output formatting

--- web_app/app.py
+++ web_app/app.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import os
-import json
 import pandas as pd
-import datetime
 
 
 app = Flask(__name__)
-model = pickle.load(open(os.getcwd() + r'/model.pkl', 'rb'))
+model = pickle.load(open(os.getcwd() + r'/../model.pkl', 'rb'))
 
 
 @app.route('/')
@@ -18,33 +16,23 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     req_dict = request.form.to_dict(flat=False)
-    # data = format_data(data)
     prediction = model.predict(pd.DataFrame.from_dict(req_dict))
-    output = prediction[0]
+    topic_map = {0: 'General', 1: 'Update related issue', 2: 'Content related query', 3: 'Technical issue',
+                 4: 'Subscription and account related issue'}
+    output = topic_map[prediction[0]]
     return render_template('index.html', prediction_text='Ticket has been classified '
                                                          'as: {}'.format(output))
 
 
 @app.route('/results', methods=['POST'])
 def results():
-    data = request.get_json(force=True)
-    data = format_data(data)
-    prediction = model.predict(pd.read_json(json.dumps(data), lines=True))
-
-    output = prediction[0]
+    req_dict = request.form.to_dict(flat=False)
+    prediction = model.predict(pd.DataFrame.from_dict(req_dict))
+    topic_map = {0: 'General', 1: 'Update related issue', 2: 'Content related query', 3: 'Technical issue',
+                 4: 'Subscription and account related issue'}
+    output = topic_map[prediction[0]]
     return jsonify(output)
 
 
-def format_data(data):
-    timestamp = datetime.datetime.now()
-    # todo generate any additional temporal features and append to data object
-    return data
-
-
-def get_cluster_name(x):
-    # todo not sure if this should live here or as a static method in our data parser
-    cluster_dict = {'cluster_id': 'cluster_name'}
-    return cluster_dict[x]
-
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
