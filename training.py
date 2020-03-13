2dd0045c884923a3b1b2775ad381302a35d98a3c
Achilleas Athanasiou Fragkoulis <achilleasatha@gmail.com>
2020-03-13 01:56:37 +0000

    Updating main

--- training.py
+++ training.py
@@ -1,17 +1,9 @@
-import pandas as pd
-import os
-from data_parser.data_parser import DataParser
 from pipeline.training_pipeline import TrainingPipeline
-from sklearn.ensemble import RandomForestClassifier
+
 
 if __name__ == "__main__":
     data_path = r'C:\Users\Achilles\PycharmProjects\data\Spotify'
-    data = pd.read_csv(data_path + r'\spotify-public-dataset.csv')
-
+    # data = DataParser(data_path).data
     random_state = 1337
-
-    model = RandomForestClassifier()
-
-    param_grid = {'hyperparams': []}
-
-    TrainingPipeline(model, data, data_path, param_grid, random_state)
+    params = {'min_df': 0.01, 'max_df': 0.25, 'ngrams': (1, 3), 'clusters': 5}
+    TrainingPipeline(data=None, params=params, random_state=random_state)
